abcf2901f51ed0f3b94329c1b2a08249ef8a8add
Alok Anand <alokanand02@gmail.com>
2013-06-18 00:49:32 +0530

    deploy changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,6 +19,7 @@ role :app, "192.168.0.106"                          # This may be the same as yo
 role :db,  "192.168.0.106", :primary => true        # This is where Rails migrations will run
 
 # if you want to clean up old releases on each deploy uncomment this:
+after 'deploy:update_code', 'deploy:symlink_config_files'
 after "deploy:restart", "deploy:cleanup" 
 
 namespace :deploy do
@@ -30,13 +31,17 @@ namespace :deploy do
     # Do nothing.
   end
 
+  desc "Symlink shared config files"
+  task :symlink_config_files do
+    run "ln -sf #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end
+
   desc "Restart Application"
   task :restart, :roles => :app do
     run "touch #{current_path}/tmp/restart.txt"
   end
 
   after 'deploy:update_code' do
-    run "#{ sudo } ln -sf #{ deploy_to }/shared/config/database.yml #{ current_path }/config/database.yml"
     run "cd #{release_path}; RAILS_ENV=production rake assets:precompile"
     run "cd #{release_path}; RAILS_ENV=production rake db:create"
     run "cd #{release_path}; RAILS_ENV=production rake db:migrate"
