a35c7bf212c40818f8388640609d573d59279ce8
Alok Anand <alokanand02@gmail.com>
2013-06-17 23:25:47 +0530

    deploy file changes affected

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,4 @@
-#require 'rvm/capistrano'
 require 'capistrano'
-#set :rvm_type, :user
-#set :rvm_ruby_string, 'ruby-2.0.0-p195@deploy_demo_app'
 set :application, "Deploy Demo App"
 set :repository,  "git://github.com/AlokAnand/deploy_demo_app.git"
 set :scm, :git
@@ -9,15 +6,11 @@ set :scm_username, "AlokAnand"
 set :branch, "master"
 set :git_enable_submodules, 1
 set :rails_env, "production"
-default_environment["RAILS_ENV"] = 'production'
-#set :rake , "/usr/bin/env/rake"
 set :deploy_to, "/var/www/192.168.0.106"
 set :deploy_via, :remote_cache
 set :user, "alok"
+set :group, "deployers"
 set :use_sudo, false
-set :ssh_options, { :forward_agent => true, :port => 22 }
-set :keep_releases, 5
-default_run_options[:pty] = true
 set :ssh_options, { :forward_agent => true, :port => 22 }
 set :keep_releases, 5
 default_run_options[:pty] = true
@@ -25,37 +18,28 @@ role :web, "192.168.0.106"                          # Your HTTP server, Apache/e
 role :app, "192.168.0.106"                          # This may be the same as your `Web` server
 role :db,  "192.168.0.106", :primary => true        # This is where Rails migrations will run
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# If you are using Passenger mod_rails uncomment this:
-namespace :rvm do
-  task :trust_rvmrc do
-    run "rvm rvmrc trust #{release_path}"
-  end
-end
-
 namespace :deploy do
-  task :start, :roles => :app do
-    run "touch #{current_path}/tmp/restart.txt"
-  end
-
-  task :stop, :roles => :app do
-    # Do nothing.
-  end
+  task :start do ; end
+  task :stop do ; end
 
   desc "Symlink shared config files"
   task :symlink_config_files do
     run "#{ sudo } ln -s #{ deploy_to }/shared/config/database.yml #{ current_path }/config/database.yml"
   end
 
-  desc "Restart Application"
-  task :restart, :roles => :app do
-    run "touch #{current_path}/tmp/restart.txt"
+  desc "Precompile assets after deploy"
+  task :precompile_assets do
+    run <<-CMD
+      cd #{ current_path } &&
+      #{ sudo } bundle exec rake assets:precompile RAILS_ENV=#{ rails_env }
+    CMD
   end
 
-  after "deploy", "rvm:trust_rvmrc"
-  #  after "deploy:update_code", "deploy:migrate"
+  desc "Restart applicaiton"
+  task :restart do
+    run "#{ try_sudo } touch #{ File.join(current_path, 'tmp', 'restart.txt') }"
+  end
+
   after 'deploy:update_code' do
     run "cd #{release_path}; RAILS_ENV=production rake assets:precompile"
     run "cd #{release_path}; RAILS_ENV=production rake db:create"
