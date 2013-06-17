c302996b4db53c98c87e86130618a6b62343acee
Alok Anand <alokanand02@gmail.com>
2013-06-17 23:54:34 +0530

    deploy file changes affected

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,18 +19,17 @@ role :app, "192.168.0.106"                          # This may be the same as yo
 role :db,  "192.168.0.106", :primary => true        # This is where Rails migrations will run
 
 namespace :deploy do
-  task :start do 
-        put "222222222222"
-     put "#{ current_path }"
-     put "2222"
-     put "#{ release_path }"
-    put "222222222222"
+  task :start, :roles => :app do
+    run "touch #{current_path}/tmp/restart.txt"
   end
-  task :stop do ; end
+ 
+  task :stop, :roles => :app do
+    # Do nothing.
+  end
 
   desc "Symlink shared config files"
   task :symlink_config_files do
-    run "#{ sudo } ln -s #{ deploy_to }/shared/config/database.yml #{ release_path }/config/database.yml"
+    run "#{ sudo } ln -s #{ deploy_to }/shared/config/database.yml #{ current_path }/config/database.yml"
   end
 
   desc "Precompile assets after deploy"
@@ -41,22 +40,18 @@ namespace :deploy do
     CMD
   end
 
-  desc "Restart applicaiton"
-  task :restart do
-    run "#{ try_sudo } touch #{ File.join(current_path, 'tmp', 'restart.txt') }"
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "touch #{current_path}/tmp/restart.txt"
   end
 
-=begin
   after 'deploy:update_code' do
     run "cd #{release_path}; RAILS_ENV=production rake assets:precompile"
     run "cd #{release_path}; RAILS_ENV=production rake db:create"
     run "cd #{release_path}; RAILS_ENV=production rake db:migrate"
   end
-=end
 
 end
 
 # if you want to clean up old releases on each deploy uncomment this:
-after "deploy:update_code", "deploy:symlink_config_files"
-after "deploy", "deploy:restart"
-after "deploy", "deploy:cleanup"
+after "deploy:symlink_config_files", "deploy:restart", "deploy:cleanup" 
