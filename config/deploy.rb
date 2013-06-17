3d6192a99b97a222fb2d5c830b2ac4512258a467
Alok Anand <alokanand02@gmail.com>
2013-06-17 18:33:48 +0530

    dploye file changed

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 require 'rvm/capistrano'
 set :rvm_type, :user
 set :rvm_ruby_string, 'ruby-2.0.0-p195@deploy_demo_app'
-set :application, "https://127.0.0.1"
+set :application, "Deploy Demo App"
 set :repository,  "git://github.com/AlokAnand/deploy_demo_app.git"
 set :scm, :git
 set :scm_username, "AlokAnand"
@@ -10,14 +10,19 @@ set :git_enable_submodules, 1
 set :rails_env, "production"
 default_environment["RAILS_ENV"] = 'production'
 #set :rake , "/usr/bin/env/rake"
-set :deploy_to, "/var/www/192.168.9.247"
+set :deploy_to, "/var/www/192.168.0.106"
 set :deploy_via, :remote_cache
 set :user, "alok"
 set :use_sudo, false
-role :web, "https://127.0.0.1"                          # Your HTTP server, Apache/etc
-role :app, "https://127.0.0.1"                          # This may be the same as your `Web` server
-role :db,  "https://127.0.0.1", :primary => true        # This is where Rails migrations will run
-role :db,  "https://127.0.0.1"
+set :ssh_options, { :forward_agent => true, :port => 22 }
+set :keep_releases, 5
+default_run_options[:pty] = true
+set :ssh_options, { :forward_agent => true, :port => 22 }
+set :keep_releases, 5
+default_run_options[:pty] = true
+role :web, "https://192.168.0.106"                          # Your HTTP server, Apache/etc
+role :app, "https://192.168.0.106"                          # This may be the same as your `Web` server
+role :db,  "https://192.168.0.106", :primary => true        # This is where Rails migrations will run
 
 # if you want to clean up old releases on each deploy uncomment this:
 after "deploy:restart", "deploy:cleanup"
@@ -41,6 +46,11 @@ namespace :deploy do
     # Do nothing.
   end
 
+  desc "Symlink shared config files"
+  task :symlink_config_files do
+    run "#{ sudo } ln -s #{ deploy_to }/shared/config/database.yml #{ current_path }/config/database.yml"
+  end
+
   desc "Restart Application"
   task :restart, :roles => :app do
     run "touch #{current_path}/tmp/restart.txt"
@@ -56,3 +66,6 @@ namespace :deploy do
 
 end
 
+after "deploy", "deploy:symlink_config_files"
+after "deploy", "deploy:restart"
+after "deploy", "deploy:cleanup"
