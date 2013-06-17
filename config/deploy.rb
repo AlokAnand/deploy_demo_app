316847cf4051b0723ebb5092a04e0e634129a07d
Alok Anand <alokanand02@gmail.com>
2013-06-17 21:48:07 +0530

    deploy changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,9 +20,9 @@ default_run_options[:pty] = true
 set :ssh_options, { :forward_agent => true, :port => 22 }
 set :keep_releases, 5
 default_run_options[:pty] = true
-role :web, "https://192.168.0.106"                          # Your HTTP server, Apache/etc
-role :app, "https://192.168.0.106"                          # This may be the same as your `Web` server
-role :db,  "https://192.168.0.106", :primary => true        # This is where Rails migrations will run
+role :web, "192.168.0.106"                          # Your HTTP server, Apache/etc
+role :app, "192.168.0.106"                          # This may be the same as your `Web` server
+role :db,  "192.168.0.106", :primary => true        # This is where Rails migrations will run
 
 # if you want to clean up old releases on each deploy uncomment this:
 after "deploy:restart", "deploy:cleanup"
